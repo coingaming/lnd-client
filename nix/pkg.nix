54133e3384bacbc76972ff51eaf57d51ba1ed1b1
Ilja Tkachuk <ilja@heathmont.net>
2020-05-04 12:40:05 +0300

    upgraded pkg.nix

--- nix/pkg.nix
+++ nix/pkg.nix
@@ -1,11 +1,12 @@
-{ mkDerivation, aeson, aeson-casing, aeson-qq, base
+{ mkDerivation, aeson, aeson-casing, aeson-qq, async, base
 , base64-bytestring, bytestring, conduit, connection
 , crypto-pubkey-openssh, crypto-pubkey-types, cryptohash-sha256
 , cryptonite, data-default, envparse, hpack, hspec, hspec-wai
 , http-client, http-client-tls, http-conduit, http-types, jose
-, katip, lens, monad-logger, monad-time, mtl, QuickCheck
-, quickcheck-instances, resource-pool, retry, scotty, stdenv, text
-, time, tls, transformers, unbounded-delays, unliftio
+, JuicyPixels, katip, lens, monad-logger, monad-time, mtl
+, persistent, qrcode-core, qrcode-juicypixels, QuickCheck
+, quickcheck-instances, resource-pool, retry, scientific, scotty
+, stdenv, text, time, tls, transformers, unbounded-delays, unliftio
 , unordered-containers, wai, warp
 }:
 mkDerivation {
@@ -13,22 +14,24 @@ mkDerivation {
   version = "0.1.0.0";
   src = ./..;
   libraryHaskellDepends = [
-    aeson aeson-casing aeson-qq base base64-bytestring bytestring
+    aeson aeson-casing aeson-qq async base base64-bytestring bytestring
     conduit connection crypto-pubkey-openssh crypto-pubkey-types
     cryptohash-sha256 cryptonite data-default envparse hspec hspec-wai
-    http-client http-client-tls http-conduit http-types jose katip lens
-    monad-logger monad-time mtl QuickCheck quickcheck-instances
-    resource-pool retry scotty text time tls transformers
+    http-client http-client-tls http-conduit http-types jose
+    JuicyPixels katip lens monad-logger monad-time mtl persistent
+    qrcode-core qrcode-juicypixels QuickCheck quickcheck-instances
+    resource-pool retry scientific scotty text time tls transformers
     unbounded-delays unliftio unordered-containers wai warp
   ];
   libraryToolDepends = [ hpack ];
   testHaskellDepends = [
-    aeson aeson-casing aeson-qq base base64-bytestring bytestring
+    aeson aeson-casing aeson-qq async base base64-bytestring bytestring
     conduit connection crypto-pubkey-openssh crypto-pubkey-types
     cryptohash-sha256 cryptonite data-default envparse hspec hspec-wai
-    http-client http-client-tls http-conduit http-types jose katip lens
-    monad-logger monad-time mtl QuickCheck quickcheck-instances
-    resource-pool retry scotty text time tls transformers
+    http-client http-client-tls http-conduit http-types jose
+    JuicyPixels katip lens monad-logger monad-time mtl persistent
+    qrcode-core qrcode-juicypixels QuickCheck quickcheck-instances
+    resource-pool retry scientific scotty text time tls transformers
     unbounded-delays unliftio unordered-containers wai warp
   ];
   prePatch = "hpack";
