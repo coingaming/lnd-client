f0f848fcf12f9e6d23b2ce768a0a3e2afd9a91bc
tkachuk-labs <tkachuk.labs@gmail.com>
2020-12-21 17:54:18 +0200

    fix nix shell

--- nix/shell.sh
+++ nix/shell.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-export IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 export VIM_BACKGROUND="${VIM_BACKGROUND:-light}"
 export VIM_COLOR_SCHEME="${VIM_COLOR_SCHEME:-PaperColor}"
-echo "host is $IP"
 echo "starting nixos container..."
-# enable for GUI apps
-# xhost + $IP
 docker run -it --rm \
-  -e DISPLAY=$IP:0 \
   -e NIXPKGS_ALLOW_BROKEN=1 \
   -e ROBOT_SSH_KEY="$ROBOT_SSH_KEY" \
   -v "$(pwd):/app" \
