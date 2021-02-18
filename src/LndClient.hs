55939f85111d7c7bb37ce1cb2097eba2c9b37608
tkachuk-labs <tkachuk.labs@gmail.com>
2021-02-18 17:53:49 +0200

    import data kinds

--- src/LndClient.hs
+++ src/LndClient.hs
@@ -4,4 +4,5 @@ import LndClient.Class as Import
 import LndClient.Data.LndEnv as Import
 import LndClient.Data.Newtype as Import
 import LndClient.Data.Type as Import
+import LndClient.Data.Kind as Import
 import LndClient.Util as Import
