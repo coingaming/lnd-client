4c5a809901173cec641e0266d611d9a82e33f64a
MikhailPrushinskiy <mikhail.prushinskiy@heathmont.net>
2021-06-28 15:38:02 +0300

    Clean up packages

--- nix/pkg.nix
+++ nix/pkg.nix
@@ -1,15 +1,13 @@
 { mkDerivation, aeson, asn1-encoding, asn1-types, async, base
 , base16-bytestring, base64-bytestring, bytestring, chronos
-, containers, cryptohash-sha256, cryptonite, data-default-class
-, deepseq, envparse, extra, grpc-haskell, grpc-haskell-core, hpack
-, hspec, http2, http2-client, http2-client-grpc
-, http2-grpc-proto-lens, http2-grpc-proto3-wire, http2-grpc-types
-, JuicyPixels, katip, lens, lifted-async, lifted-base, microlens
-, network, network-bitcoin, pem, persistent, process-extras
-, proto-lens, proto-lens-runtime, proto3-suite, proto3-wire
-, qrcode-core, qrcode-juicypixels, scientific, stdenv, stm
-, template-haskell, text, tls, unbounded-delays, universum
-, unliftio, vector, warp-grpc, warp-tls, x509
+, containers, cryptohash-sha256, cryptonite, deepseq, envparse
+, extra, grpc-haskell, grpc-haskell-core, hpack, hspec
+, http2-client, http2-client-grpc, http2-grpc-proto-lens
+, http2-grpc-types, JuicyPixels, katip, microlens, network-bitcoin
+, pem, persistent, proto-lens, proto-lens-runtime, proto3-suite
+, proto3-wire, qrcode-core, qrcode-juicypixels, scientific, stdenv
+, stm, template-haskell, text, unbounded-delays, universum
+, unliftio, vector, x509
 }:
 mkDerivation {
   pname = "lnd-client";
@@ -18,27 +16,23 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson asn1-encoding asn1-types async base base16-bytestring
     base64-bytestring bytestring chronos containers cryptohash-sha256
-    cryptonite data-default-class deepseq envparse extra grpc-haskell
-    grpc-haskell-core hspec http2 http2-client http2-client-grpc
-    http2-grpc-proto-lens http2-grpc-proto3-wire http2-grpc-types
-    JuicyPixels katip lens lifted-async lifted-base microlens network
-    network-bitcoin pem persistent process-extras proto-lens
-    proto-lens-runtime proto3-suite proto3-wire qrcode-core
-    qrcode-juicypixels scientific stm template-haskell text tls
-    unbounded-delays universum unliftio vector warp-grpc warp-tls x509
+    cryptonite deepseq envparse extra grpc-haskell grpc-haskell-core
+    hspec http2-client http2-client-grpc http2-grpc-proto-lens
+    http2-grpc-types JuicyPixels katip microlens network-bitcoin pem
+    persistent proto-lens proto-lens-runtime proto3-suite proto3-wire
+    qrcode-core qrcode-juicypixels scientific stm template-haskell text
+    unbounded-delays universum unliftio vector x509
   ];
   libraryToolDepends = [ hpack ];
   testHaskellDepends = [
     aeson asn1-encoding asn1-types async base base16-bytestring
     base64-bytestring bytestring chronos containers cryptohash-sha256
-    cryptonite data-default-class deepseq envparse extra grpc-haskell
-    grpc-haskell-core hspec http2 http2-client http2-client-grpc
-    http2-grpc-proto-lens http2-grpc-proto3-wire http2-grpc-types
-    JuicyPixels katip lens lifted-async lifted-base microlens network
-    network-bitcoin pem persistent process-extras proto-lens
-    proto-lens-runtime proto3-suite proto3-wire qrcode-core
-    qrcode-juicypixels scientific stm template-haskell text tls
-    unbounded-delays universum unliftio vector warp-grpc warp-tls x509
+    cryptonite deepseq envparse extra grpc-haskell grpc-haskell-core
+    hspec http2-client http2-client-grpc http2-grpc-proto-lens
+    http2-grpc-types JuicyPixels katip microlens network-bitcoin pem
+    persistent proto-lens proto-lens-runtime proto3-suite proto3-wire
+    qrcode-core qrcode-juicypixels scientific stm template-haskell text
+    unbounded-delays universum unliftio vector x509
   ];
   prePatch = "hpack";
   homepage = "https://github.com/githubuser/lnd-client#readme";
