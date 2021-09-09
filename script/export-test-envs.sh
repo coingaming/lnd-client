2cace4a97f2d5e5ab507755f1328fdcd74d7b884
drownbes <drownbes@gmail.com>
2021-09-09 11:57:12 +0300

    fix

--- script/export-test-envs.sh
+++ script/export-test-envs.sh
@@ -17,9 +17,9 @@ alias bitcoin-cli="bitcoin-cli -datadir=$BTCD_DIR"
 #
 
 export LND_MERCHANT_DIR="$THIS_DIR/.lnd-merchant"
-alias lncli-merchant="lncli -n regtest --lnddir=$LND_MERCHANT_DIR --rpcserver=localhost:10009"
+alias lncli-merchant="lncli -n regtest --lnddir=$LND_MERCHANT_DIR --rpcserver=127.0.0.1:10009"
 export LND_CUSTOMER_DIR="$THIS_DIR/.lnd-customer"
-alias lncli-customer="lncli -n regtest --lnddir=$LND_CUSTOMER_DIR --rpcserver=localhost:11009"
+alias lncli-customer="lncli -n regtest --lnddir=$LND_CUSTOMER_DIR --rpcserver=127.0.0.1:11009"
 
 #
 # lnd-client
@@ -33,7 +33,7 @@ export LND_CLIENT_ENV_DATA="
     \"lnd_wallet_password\":\"developer\",
     \"lnd_tls_cert\":\"$LND_TLS_CERT\",
     \"lnd_hex_macaroon\":\"0201036C6E6402CF01030A10634D5C8D3227E9F63529F82690C1898E1201301A160A0761646472657373120472656164120577726974651A130A04696E666F120472656164120577726974651A170A08696E766F69636573120472656164120577726974651A160A076D657373616765120472656164120577726974651A170A086F6666636861696E120472656164120577726974651A160A076F6E636861696E120472656164120577726974651A140A057065657273120472656164120577726974651A120A067369676E6572120867656E657261746500000620EB31C7413A5A44D14705852F8C0CA399104658C40AC866918C1D4B981DF2E71E\",
-    \"lnd_host\":\"localhost\",
+    \"lnd_host\":\"127.0.0.1\",
     \"lnd_port\":10009,
     \"lnd_cipher_seed_mnemonic\":[
                   \"absent\",
--- script/generate-tls-cert.sh
+++ script/generate-tls-cert.sh
@@ -3,7 +3,7 @@
 (
   cd ./.lnd
   openssl ecparam -genkey -name prime256v1 -out tls.key
-  openssl req -new -sha256 -key tls.key -out csr.csr -subj '/CN=localhost/O=lnd'
-  openssl req -x509 -sha256 -days 36500 -key tls.key -in csr.csr -out tls.cert
+  openssl req -new -sha256 -key tls.key -out csr.csr -subj '/CN=localhost/O=lnd' -addext 'subjectAltName = IP:127.0.0.1'
+  openssl req -x509 -sha256 -days 36500 -key tls.key -in csr.csr -out tls.cert -addext 'subjectAltName = IP:127.0.0.1'
   rm csr.csr
 )
