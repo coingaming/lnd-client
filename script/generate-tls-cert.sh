dd7783f628954481270e23bbe5ab3ed873553059
Artem Markov <drownbes@gmail.com>
2021-10-11 15:13:56 +0300

    default verification hooks (#111)
    * default verification hooks
    
    * Fix naming

--- script/generate-tls-cert.sh
+++ script/generate-tls-cert.sh
@@ -4,7 +4,7 @@ set -e
 (
   cd ./.lnd
   openssl ecparam -genkey -name prime256v1 -out tls.key
-  openssl req -new -sha256 -key tls.key -out csr.csr -subj '/CN=localhost/O=lnd' -addext 'subjectAltName = IP:127.0.0.1'
+  openssl req -new -sha256 -key tls.key -out csr.csr -subj '/CN=127.0.0.1/O=lnd' -addext 'subjectAltName = IP:127.0.0.1'
   openssl req -x509 -sha256 -days 36500 -key tls.key -in csr.csr -out tls.cert -addext 'subjectAltName = IP:127.0.0.1'
   rm csr.csr
 )
