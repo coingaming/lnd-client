9b64111f469aebc78f1b115d32da9eeb286cdef8
Mikhail Prushinskiy <mikhail.prushinskiy@heathmont.net>
2021-12-02 10:40:33 +0200

    Update coverage calculation

--- script/coverage.sh
+++ script/coverage.sh
@@ -2,9 +2,13 @@
 
 echo "==============================Coverage================================="
 tixdir=$out
-echo "Tixdir is: $tixdir"
 hpcdir=$(echo $buildInputs | tr " " "\n" | grep lnd-client-0.1.0.0)
-echo "HPCdir is: $hpcdir"
 cov=$(hpc report --hpcdir=$hpcdir/share/hpc/vanilla/mix/lnd-client-0.1.0.0 $tixdir/share/hpc/vanilla/tix/lnd-client-test/lnd-client-test.tix --exclude=Proto.InvoiceGrpc --exclude=Proto.InvoiceGrpc_Fields --exclude=Proto.LndGrpc --exclude=Proto.LndGrpc_Fields --exclude=Proto.RouterGrpc --exclude=Proto.RouterGrpc_Fields --exclude=Proto.WalletUnlockerGrpc --exclude=Proto.WalletUnlockerGrpc_Fields | grep "expressions used" | grep -oP '\d+(?=%)')
 echo "Test coverage is: $cov%"
+if [[ $cov -lt 80 ]]
+then
+  echo "Coverage is too low, failing..."
+  exit 0
+fi
+echo "Coverage is good."
 echo "==============================/Coverage================================="
